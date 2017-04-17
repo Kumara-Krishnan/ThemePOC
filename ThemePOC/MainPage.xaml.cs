5711d384a0b52ce794189ba8e6f52e52e57a2e95
Kumara Krishnan <kumarakrishnanps@gmail.com>
2017-04-17 13:07:00 +0530

    Added Themes

--- ThemePOC/MainPage.xaml.cs
+++ ThemePOC/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ThemePOC.Views;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +27,42 @@ namespace ThemePOC
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random random;
         public MainPage()
         {
             this.InitializeComponent();
+            random = new Random();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ThemeChanger_Click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.Resources["BackgroundBrush"] as SolidColorBrush).Color=(Windows.UI.Color.FromArgb(Convert.ToByte(random.Next(255)), Convert.ToByte(random.Next(255)), Convert.ToByte(random.Next(255)), Convert.ToByte(random.Next(255))));
+        }
+
+        private async Task CreateNewViewAsync()
+        {
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(MainPage), null);
+                Window.Current.Content = frame;
+                // You have to activate the window in order to show it later.
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+        }
+
+        private void Sample_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewViewAsync();
         }
     }
 }
